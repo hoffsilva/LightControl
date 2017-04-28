b6db2b5c293ff103de9e29536c26672fe263080b
hoffsilva <sardenta1>
2017-04-28 00:16:46 -0300

    Iniciod a criacao da tela do jardim

--- app/src/main/java/com/example/hoffsilva/lightcontrol/MainActivity.java
+++ app/src/main/java/com/example/hoffsilva/lightcontrol/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hoffsilva.lightcontrol;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
     }
 
     @Override
@@ -40,4 +43,20 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void callGardenActivity(View view) {
+        Intent intent = new Intent(MainActivity.this, GardenActivity.class);
+        startActivity(intent);
+    }
+
+    public void callBedRoomActivity(View view) {
+        Intent intent = new Intent(MainActivity.this, BedRoomActivity.class);
+        startActivity(intent);
+    }
+
+    public void callRoomActivity(View view) {
+        Intent intent = new Intent(MainActivity.this, RoomActivity.class);
+        startActivity(intent);
+    }
+
 }
