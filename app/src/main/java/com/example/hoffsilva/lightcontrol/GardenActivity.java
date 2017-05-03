c752a90a7a60223cedde5d9f1183f87f43538227
hoffsilva <sardenta1>
2017-05-03 17:31:39 -0300

    tela de jardim

--- app/src/main/java/com/example/hoffsilva/lightcontrol/GardenActivity.java
+++ app/src/main/java/com/example/hoffsilva/lightcontrol/GardenActivity.java
@@ -2,6 +2,8 @@ package com.example.hoffsilva.lightcontrol;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GardenActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class GardenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_garden);
+
+        final Button setor1 = (Button) findViewById(R.id.setor1);
+        setor1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                    setBgButton(setor1);
+                }
+        });
+    }
+
+    public void setBgButton(Button botao) {
+        botao.setBackgroundResource(R.drawable.light_on);
     }
 }
